2a14fba39e72b5a51695a1f20bce9de9f1f4905b
unknown <jorge.aldeleon@gmail.com>
2025-04-09 20:46:58 -0300

    se agrego card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+
+const Card = () => {
+  return (
+    <div className="card">
+      <article className="card__article">
+      <div className="card__image-container">
+        <img className="card__image" src="foto" alt="texto foto">
+      </div>
+      <div className="card__content">
+        <h2 className="card__heading">Nombre</h2>
+        <div className="card__description">
+          <p>descripcion</p>
+        </div>
+      </div>        
+    </article>
+</div>
+  )
+}
+
+export default Card
