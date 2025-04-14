1693bb827c52746fc0fcb5ee5ce5e9fcf57817da
unknown <jorge.aldeleon@gmail.com>
2025-04-14 20:08:17 -0300

    se añadieron los componentes Tabla y TablaFila

--- src/components/components-alta/Tabla.jsx
+++ src/components/components-alta/Tabla.jsx
@@ -1,8 +1,26 @@
-
+import TablaFila from "./TablaFila"
+import './Tabla.scss'
 
 const Tabla = () => {
   return (
-    <div>Tabla</div>
+    <table className="tabla-alta">
+       <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Stock</th>
+            <th>Marca</th>
+            <th>Categoria/</th>
+            <th>Detalles</th>
+            <th>Foto</th>
+            <th>Envío</th>
+            <th>Acciones</th>
+        </tr>
+       </thead>
+<tbody>
+<TablaFila/>
+</tbody>
+    </table>
   )
 }
 
--- src/components/components-alta/TablaFila.jsx
+++ src/components/components-alta/TablaFila.jsx
@@ -2,7 +2,21 @@
 
 const TablaFila = () => {
   return (
-    <div>TablaFila</div>
+    <tr>
+        <td>nombre</td>
+        <td>precio</td>
+        <td>stock</td>
+        <td>marca</td>
+        <td>categoria</td>
+        <td>detalles</td>
+        <td>foto</td>
+        <td>envío</td>
+        <td>
+            <button>Ver</button>
+            <button>Editar</button>
+            <button>Borrar</button>
+        </td>
+    </tr>
   )
 }
 
